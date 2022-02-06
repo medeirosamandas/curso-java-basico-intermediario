62354c4e7a77ae64342df2780c54ff132034d202
Amanda Medeiros <medeiros.amandas@gmail.com>
2022-02-06 20:04:43 -0300

    colocando comentário na aula 05

--- eclipse/curso-java-basico/src/com/amanda/cursojava/aula10/Variaveis.java
+++ eclipse/curso-java-basico/src/com/amanda/cursojava/aula10/Variaveis.java
@@ -3,6 +3,7 @@ package com.amanda.cursojava.aula10;
 public class Variaveis {
 
 	public static void main(String[] args) {
+		// convenção Java!
 		int idade = 20;
 		String nome;
 		String nomeDoMeuCachorro;
