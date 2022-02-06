1aa704b31686731781ed98ae4b242bfbc5f9f5d2
Amanda Medeiros <medeiros.amandas@gmail.com>
2022-02-06 19:07:42 -0300

    Aula 10

---
+++ eclipse/curso-java-basico/src/com/amanda/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.amanda.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		String nome;
+		String nomeDoMeuCachorro;
+		
+		idade = 25;
+		
+		System.out.println("idade = " + idade);
+
+	}
+
+}
