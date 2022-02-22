a2e058ad4911f6d471236d70f40ebc30969c39a7
Amanda Medeiros <medeiros.amandas@gmail.com>
2022-02-22 09:55:17 -0300

    Aula 22

---
+++ eclipse/curso-java-basico/src/com/amanda/cursojava/aula22/DebugEclipse.java
@@ -0,0 +1,15 @@
+package com.amanda.cursojava.aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+
+
+		int[] notas = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			notas[i] = i;
+		}
+	}
+
+}
